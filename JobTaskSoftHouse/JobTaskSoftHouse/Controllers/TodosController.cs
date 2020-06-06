ec4eb5bc35fccaf87fcc0d5e7ffb28194c555c0a
sanjin1993 <sanjin.zacinovic@edu.fit.ba>
2020-06-06 19:45:56 +0200

    testing http client get asyn method, fetching test api data

--- JobTaskSoftHouse/JobTaskSoftHouse/Controllers/TodosController.cs
+++ JobTaskSoftHouse/JobTaskSoftHouse/Controllers/TodosController.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using JobTaskSoftHouse.Models;
+using JobTaskSoftHouse.Util;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,10 +12,21 @@ namespace JobTaskSoftHouse.Controllers
 {
     public class TodosController : Controller
     {
+
+        WebAPIHelper todosService = new WebAPIHelper("https://jsonplaceholder.typicode.com/todos", "todos");
         // GET: Todos
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+
+            HttpResponseMessage response = todosService.GetResponse();
+            List<Todo> todos = new List<Todo>();
+
+            if (response.IsSuccessStatusCode)
+            {
+                todos = response.Content.ReadAsAsync<List<Todo>>().Result;
+            }
+
+            return View(todos);
         }
 
         // GET: Todos/Details/5
---
+++ JobTaskSoftHouse/JobTaskSoftHouse/Models/Todo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobTaskSoftHouse.Models
+{
+    public class Todo
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public bool completed { get; set; }
+
+    }
+}
